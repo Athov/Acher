a25a175f21b0ee62afd211bbb2a894ce1e2910df
Atanas Harapov <atanas.harapov@abv.bg>
2019-01-20 17:40:42 +0200

    Create Response.php

---
+++ core/Classes/Response.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * This file is part of the Acher framework.
+ *
+ * (c) Atanas Harapov <atanas.harapov@abv.bg>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Core\Classes;
+
+/**
+ * Response class.
+ *
+ * @author Atanas Harapov <atanas.harapov@abv.bg>
+ */
+class Response
+{
+}
